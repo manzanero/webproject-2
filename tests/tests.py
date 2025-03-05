cdc4334ba3718aeff2177a8a15226b49ada7a558
alejandro.manzanero <alejandro.manzanero@mtp.es>
2025-03-05 19:15:55 +0100

    html

--- tests/tests.py
+++ tests/tests.py
@@ -5,20 +5,22 @@ import requests
 
 class MyTestCase(unittest.TestCase):
 
+    url = "https://alejandromanzanero.pythonanywhere.com/"
+
     def test_sumar(self):
-        respuesta = requests.get("http://localhost/calculadora/2/sumar/2")
+        respuesta = requests.get(self.url + "calculadora/2/sumar/2")
         assert respuesta.text == "4", f"resultado invalido {respuesta.text}"
 
     def test_restar(self):
-        respuesta = requests.get("http://localhost/calculadora/2/restar/2")
+        respuesta = requests.get(self.url + "calculadora/2/restar/2")
         assert respuesta.text == "0", f"resultado invalido {respuesta.text}"
 
     def test_multiplicar(self):
-        respuesta = requests.get("http://localhost/calculadora/2/multiplicar/2")
+        respuesta = requests.get(self.url + "calculadora/2/multiplicar/2")
         assert respuesta.text == "4", f"resultado invalido {respuesta.text}"
 
     def test_dividir(self):
-        respuesta = requests.get("http://localhost/calculadora/2/dividir/2")
+        respuesta = requests.get(self.url + "calculadora/2/dividir/2")
         assert float(respuesta.text) == float("1"), f"resultado invalido {respuesta.text}"
 
 if __name__ == '__main__':
